1de31ba7fc55eb57f0525fe2b5ca01dfdb227dd1
Adityaojas <45658478+Adityaojas@users.noreply.github.com>
2020-07-18 21:34:52 +0530

    Delete imagetoarraypreprocessor.py

--- char74K/mymodule/imagetoarraypreprocessor.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.preprocessing.image import img_to_array
-
-class ImageToArrayPreprocessor:
-    
-    def __init__(self,  dataFormat = None):
-        self.dataFormat = dataFormat
-        
-    def preprocess(self, image):
-        return img_to_array(image, self.dataFormat)
